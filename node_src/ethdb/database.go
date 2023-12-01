1de0cdcd779713effdbe1a7c394acb6a421f49c5
hide001 <shubhamswaraj1@gmail.com>
2023-12-01 17:13:24 +0530

    testing new geth version

--- node_src/ethdb/database.go
+++ node_src/ethdb/database.go
@@ -37,8 +37,8 @@ type KeyValueWriter interface {
 	Delete(key []byte) error
 }
 
-// KeyValueStater wraps the Stat method of a backing data store.
-type KeyValueStater interface {
+// Stater wraps the Stat method of a backing data store.
+type Stater interface {
 	// Stat returns a particular internal stat of the database.
 	Stat(property string) (string, error)
 }
@@ -60,16 +60,15 @@ type Compacter interface {
 type KeyValueStore interface {
 	KeyValueReader
 	KeyValueWriter
-	KeyValueStater
 	Batcher
 	Iteratee
+	Stater
 	Compacter
-	Snapshotter
 	io.Closer
 }
 
-// AncientReaderOp contains the methods required to read from immutable ancient data.
-type AncientReaderOp interface {
+// AncientReader contains the methods required to read from immutable ancient data.
+type AncientReader interface {
 	// HasAncient returns an indicator whether the specified data exists in the
 	// ancient store.
 	HasAncient(kind string, number uint64) (bool, error)
@@ -79,30 +78,25 @@ type AncientReaderOp interface {
 
 	// AncientRange retrieves multiple items in sequence, starting from the index 'start'.
 	// It will return
-	//   - at most 'count' items,
-	//   - if maxBytes is specified: at least 1 item (even if exceeding the maxByteSize),
-	//     but will otherwise return as many items as fit into maxByteSize.
-	//   - if maxBytes is not specified, 'count' items will be returned if they are present
+	//  - at most 'count' items,
+	//  - at least 1 item (even if exceeding the maxBytes), but will otherwise
+	//   return as many items as fit into maxBytes.
 	AncientRange(kind string, start, count, maxBytes uint64) ([][]byte, error)
 
 	// Ancients returns the ancient item numbers in the ancient store.
 	Ancients() (uint64, error)
 
-	// Tail returns the number of first stored item in the freezer.
-	// This number can also be interpreted as the total deleted item numbers.
-	Tail() (uint64, error)
-
 	// AncientSize returns the ancient size of the specified category.
 	AncientSize(kind string) (uint64, error)
 }
 
-// AncientReader is the extended ancient reader interface including 'batched' or 'atomic' reading.
-type AncientReader interface {
-	AncientReaderOp
+// AncientBatchReader is the interface for 'batched' or 'atomic' reading.
+type AncientBatchReader interface {
+	AncientReader
 
 	// ReadAncients runs the given read operation while ensuring that no writes take place
 	// on the underlying freezer.
-	ReadAncients(fn func(AncientReaderOp) error) (err error)
+	ReadAncients(fn func(AncientReader) error) (err error)
 }
 
 // AncientWriter contains the methods required to write to immutable ancient data.
@@ -112,24 +106,11 @@ type AncientWriter interface {
 	// The integer return value is the total size of the written data.
 	ModifyAncients(func(AncientWriteOp) error) (int64, error)
 
-	// TruncateHead discards all but the first n ancient data from the ancient store.
-	// After the truncation, the latest item can be accessed it item_n-1(start from 0).
-	TruncateHead(n uint64) (uint64, error)
-
-	// TruncateTail discards the first n ancient data from the ancient store. The already
-	// deleted items are ignored. After the truncation, the earliest item can be accessed
-	// is item_n(start from 0). The deleted items may not be removed from the ancient store
-	// immediately, but only when the accumulated deleted data reach the threshold then
-	// will be removed all together.
-	TruncateTail(n uint64) (uint64, error)
+	// TruncateAncients discards all but the first n ancient data from the ancient store.
+	TruncateAncients(n uint64) error
 
 	// Sync flushes all in-memory ancient store data to disk.
 	Sync() error
-
-	// MigrateTable processes and migrates entries of a given table to a new format.
-	// The second argument is a function that takes a raw entry and returns it
-	// in the newest format.
-	MigrateTable(string, func([]byte) ([]byte, error)) error
 }
 
 // AncientWriteOp is given to the function argument of ModifyAncients.
@@ -141,19 +122,11 @@ type AncientWriteOp interface {
 	AppendRaw(kind string, number uint64, item []byte) error
 }
 
-// AncientStater wraps the Stat method of a backing data store.
-type AncientStater interface {
-	// AncientDatadir returns the path of root ancient directory. Empty string
-	// will be returned if ancient store is not enabled at all. The returned
-	// path can be used to construct the path of other freezers.
-	AncientDatadir() (string, error)
-}
-
 // Reader contains the methods required to read data from both key-value as well as
 // immutable ancient data.
 type Reader interface {
 	KeyValueReader
-	AncientReader
+	AncientBatchReader
 }
 
 // Writer contains the methods required to write data to both key-value as well as
@@ -163,17 +136,10 @@ type Writer interface {
 	AncientWriter
 }
 
-// Stater contains the methods required to retrieve states from both key-value as well as
-// immutable ancient data.
-type Stater interface {
-	KeyValueStater
-	AncientStater
-}
-
 // AncientStore contains all the methods required to allow handling different
 // ancient data stores backing immutable chain data store.
 type AncientStore interface {
-	AncientReader
+	AncientBatchReader
 	AncientWriter
 	io.Closer
 }
@@ -187,6 +153,5 @@ type Database interface {
 	Iteratee
 	Stater
 	Compacter
-	Snapshotter
 	io.Closer
 }
