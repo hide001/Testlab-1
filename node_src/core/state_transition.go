3f9d85c8c8775010e999cbf8c6aff55daec881d1
hide001 <shubhamswaraj1@gmail.com>
2023-12-07 15:27:41 +0530

    bug fixes

--- node_src/core/state_transition.go
+++ node_src/core/state_transition.go
@@ -24,7 +24,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	cmath "github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/consensus"
+	// "github.com/ethereum/go-ethereum/consensus"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/params"
